1fa0f8434b7dc846e74a646ff2b40a06d6025997
TzvikaShtainer <TzvikaShtainer@gmail.com>
2025-02-21 13:22:18 +0200

    Adding Timer For Spawning

--- Assets/Scripts/VisualLayer/MergeItems/SpawnLogic/SpawnLogic.cs
+++ Assets/Scripts/VisualLayer/MergeItems/SpawnLogic/SpawnLogic.cs
@@ -8,13 +8,21 @@ namespace VisualLayer.MergeItems.SpawnLogic
         [Inject]
         private Item.Factory _itemFactory;
         
+        private float _lastTimeFire;
         
         public void Spawn()
         {
             //mybe add timer fow spawn
+            var isInDelay = Time.time - _lastTimeFire < 0.5; //0.5 just for now
+            if (isInDelay)
+            {
+                return;
+            }
             
             
             var itemToSpawn = _itemFactory.Create();
+            
+            _lastTimeFire = Time.time;
         }
     }
 }
