f257258a34f59699ab93184797ef12f5500d8c37
TzvikaShtainer <TzvikaShtainer@gmail.com>
2025-03-01 14:51:38 +0200

    Loader Working - Not Trasformming Between Scene For Now

--- Assets/Scripts/VisualLayer/GamePlay/Handlers/HudBackClickHandler.cs
+++ Assets/Scripts/VisualLayer/GamePlay/Handlers/HudBackClickHandler.cs
@@ -20,6 +20,18 @@ namespace VisualLayer.GamePlay.Handlers
             _loader.SetProgress(0.2f, "Loader 20%");
             await UniTask.Delay(1500);
             _loader.SetProgress(0.5f, "Loader 50%");
+            //unload gameplay lvl scene
+            //await _scenesService.UnloadLevelScene(_currentLevelType);
+            
+            await UniTask.Delay(1500);
+            _loader.SetProgress(1f, "Loader 100%");
+            
+            //load lvl selection scene
+            //await _scenesService.LoadInfraSceneIfNotLoaded(InfraScreenType.SelectLevel);
+            
+            
+            await UniTask.Delay(1500);
+            await _loader.FadeOut();
         }
     }
 }
