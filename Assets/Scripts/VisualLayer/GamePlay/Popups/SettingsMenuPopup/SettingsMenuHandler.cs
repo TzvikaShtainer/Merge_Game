787b8e3bbad6472864a5f703dac57eedbc73a17b
TzvikaShtainer <TzvikaShtainer@gmail.com>
2025-06-17 07:50:32 +0300

    Fixing Loader % And Visuals

--- Assets/Scripts/VisualLayer/GamePlay/Popups/SettingsMenuPopup/SettingsMenuHandler.cs
+++ Assets/Scripts/VisualLayer/GamePlay/Popups/SettingsMenuPopup/SettingsMenuHandler.cs
@@ -38,24 +38,24 @@ namespace VisualLayer.GamePlay.Popups.MusicMenuPopup
         {
             _loader.ResetData();
             await _loader.FadeIn();
-            _loader.SetProgress(0.1f, "Going to the level selection");
+            _loader.SetProgress(0.2f, "Loading Level 20%");
             await UniTask.Delay(TimeSpan.FromSeconds(1));
 
             
             //unload gameplay lvl scene
-            _loader.SetProgress(0.2f, "Unloading the level");
+            _loader.SetProgress(0.5f, "Loading Level 50%");
             await _scenesService.UnloadLevelScene(_currentLevelType);
             await UniTask.Delay(TimeSpan.FromSeconds(1));
             
             //load lvl selection scene
-            _loader.SetProgress(0.4f, "Loading levels list");
+            _loader.SetProgress(0.7f, "Loading Level 70%");
             await _scenesService.LoadLevelSceneIfNotLoaded(GameLevelType.GamePlay);
             
             await _scenesService.LoadInfraSceneIfNotLoaded(InfraScreenType.Loader);
             await _scenesService.LoadInfraSceneIfNotLoaded(InfraScreenType.GamePopups);
             
             await UniTask.Delay(TimeSpan.FromSeconds(1));
-            _loader.SetProgress(0.9f, "Completing");
+            _loader.SetProgress(1f, "Loading Level 100%");
             await UniTask.Delay(TimeSpan.FromSeconds(1));
             await _loader.FadeOut();
         }
--- Assets/Scripts/VisualLayer/Loader/LoaderInstaller.cs
+++ Assets/Scripts/VisualLayer/Loader/LoaderInstaller.cs
@@ -58,15 +58,16 @@ namespace VisualLayer.Loader
         {
             _loader.ResetData();
             await _loader.FadeIn();
+            
             await UniTask.Delay(500);
             _loader.SetProgress(0.2f, "Loading Level 20%");
             
             await _serverService.Login();
+            
             await _dataLayer.Balances.LoadFromServer();
             
             await UniTask.Delay(1000);
             await _scenesService.LoadInfraSceneIfNotLoaded(InfraScreenType.GamePopups);
-            
             
             await UniTask.Delay(1000);
             _loader.SetProgress(0.5f, "Loading Level 50%");
@@ -74,9 +75,12 @@ namespace VisualLayer.Loader
             await _scenesService.LoadLevelSceneIfNotLoaded(GameLevelType.GamePlay);
             
             await UniTask.DelayFrame(100);
+            _loader.SetProgress(0.7f, "Loading Level 70%");
+            
             await _abilityManager.LoadFromServer();
             
             await UniTask.Delay(500);
+            
             await _saveSystem.Load();
             
             _loader.SetProgress(1f, "Loading Level 100%");
