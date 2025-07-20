35dda8cee53b3c75d2581dab8015c3310533548f
TzvikaShtainer <TzvikaShtainer@gmail.com>
2025-07-20 09:35:07 +0300

    Fix CurrentScore Bug 2

--- Assets/Scripts/ServiceLayer/SaveSystem/SaveSystemGameObject.cs
+++ Assets/Scripts/ServiceLayer/SaveSystem/SaveSystemGameObject.cs
@@ -9,12 +9,17 @@ namespace ServiceLayer.SaveSystem
         [Inject] 
         private ISaveSystem _saveService;
         
+        [Inject]
+        private IDataLayer _dataLayer;
+        
         private void OnApplicationPause(bool pause)
         {
             if (pause)
             {
                 //Debug.Log("pause");
                 _saveService.Save();
+                _dataLayer.Balances.SetCurrentScore(_dataLayer.Balances.CurrentScore);
+
             }
         }
 
@@ -22,6 +27,8 @@ namespace ServiceLayer.SaveSystem
         {
             //Debug.Log("OnApplicationQuit");
             _saveService.Save();
+            _dataLayer.Balances.SetCurrentScore(_dataLayer.Balances.CurrentScore);
+
         }
     }
     
--- Assets/Scripts/VisualLayer/GamePlay/Score/SignalBasedGameScoreCalculator.cs
+++ Assets/Scripts/VisualLayer/GamePlay/Score/SignalBasedGameScoreCalculator.cs
@@ -30,7 +30,6 @@ namespace VisualLayer.GamePlay.Score
         private void ItemMergedSignalHandler()
         {
             _dataLayer.Balances.AddCurrentScore(_params.ItemsMerged); //multi by lvl merge mybe?
-            _dataLayer.Balances.SetCurrentScore(_dataLayer.Balances.CurrentScore);
 
             if ( _dataLayer.Balances.CurrentScore > _dataLayer.Balances.HighScore)
             {
