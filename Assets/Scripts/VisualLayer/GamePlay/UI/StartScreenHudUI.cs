28fb7bc24267f5648721276437146b1f2ea75723
TzvikaShtainer <TzvikaShtainer@gmail.com>
2025-10-01 16:25:51 +0300

    Adding Coins In Start Screen

---
+++ Assets/Scripts/VisualLayer/GamePlay/UI/StartScreenHudUI.cs
@@ -0,0 +1,30 @@
+﻿using DataLayer;
+using TMPro;
+using UnityEngine;
+using Zenject;
+
+namespace VisualLayer.GamePlay.UI
+{
+    public class StartScreenHudUI : MonoBehaviour
+    {
+        [SerializeField] 
+        private TextMeshProUGUI _coinsBalanceText;
+        
+        [Inject]
+        private IDataLayer _dataLayer;
+        
+        private void Start()
+        {
+            InitializeView();
+        }
+        private void InitializeView()
+        {
+            _dataLayer.Balances.CoinsBalanceChanged += SyncUiWithData;
+        }
+
+        private void SyncUiWithData()
+        {
+            _coinsBalanceText.text = _dataLayer.Balances.Coins.ToString();
+        }
+    }
+}
