74bb5df99c20f78c054f2f0205e12b97a37d7a45
TzvikaShtainer <TzvikaShtainer@gmail.com>
2026-03-02 11:55:51 +0200

    Add Delay To Start Btn For Hub MSG To Pop

--- Assets/Scripts/VisualLayer/GamePlay/Buttons/EnterLevelButton.cs
+++ Assets/Scripts/VisualLayer/GamePlay/Buttons/EnterLevelButton.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
 using VisualLayer.GamePlay.Handlers;
 using Zenject;
 
@@ -8,10 +10,29 @@ namespace VisualLayer.GamePlay.Buttons
     {
         [Inject]
         private IStartGameClickHandler _enterLevelHandler;
-        
-        
+
+        private bool _isBlocked = true;
+        private int _timeToBlockBtnInSec = 7;
+
+        private void Start()
+        {
+            StartCooldown().Forget();
+        }
+
+        private async UniTask StartCooldown()
+        {
+            _isBlocked = true;
+            
+            await UniTask.Delay(TimeSpan.FromSeconds(_timeToBlockBtnInSec), DelayType.Realtime);
+            _isBlocked = false;
+        }
+
+
         public void OnClick()
         {
+            if (_isBlocked)
+                return;
+            
             base.OnClick();
             _enterLevelHandler.Execute();
         }
